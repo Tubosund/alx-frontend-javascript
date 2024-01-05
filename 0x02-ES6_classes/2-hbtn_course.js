d97a0c32d7c5d979a12f0e8a855e8553c97d2cb5
Olatunbosun Oladayo <tubosund@gmail.com>
2024-01-05 08:14:30 -0800

    first

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,45 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(val) {
+    if (typeof val !== 'string') {
+      throw TypeError('Name must be a string');
+    }
+    this._name = val;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(val) {
+    if (typeof val !== 'number') {
+      throw TypeError('Length must be a number');
+    }
+    this._length = val;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(val) {
+    if (!Array.isArray(val)) {
+      throw TypeError('Students must be a array of strings');
+    }
+    for (const student of val) {
+      if (typeof student !== 'string') {
+        throw TypeError('Students must be a array of strings');
+      }
+    }
+    this._students = val;
+  }
+}
