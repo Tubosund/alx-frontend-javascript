109997d83429243866a9695c0ac674db956a4453
Olatunbosun Oladayo <tubosund@gmail.com>
2024-01-09 16:18:44 -0800

    hk

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,25 @@
+/**
+ * cleanSet - These search for a startString and appends
+ * each matches suffixes to form a word.
+ *
+ * @param {set} - set, to use
+ * @param {String} - startString, the string to match
+ * @returns {String}
+ */
+
+export default function (set, startString = '') {
+  const len = startString.length;
+  let str = '';
+
+  if (set instanceof Set && typeof startString === 'string') {
+    for (const el of set) {
+      if (typeof el === 'string') {
+        if (startString === el.slice(0, len) && len > 0) {
+          str += `${el.slice(len)}-`;
+        }
+      }
+    }
+    str = str.slice(0, str.length - 1);
+  }
+  return str;
+}
